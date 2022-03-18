0a99283d356776a3e0a5ee871ff27d26c38ec799
jeeasistio <jeeasistio08@gmail.com>
2022-03-18 11:46:59 +0800

    env added

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    GOOGLE_USER: 'portfolio.jee@gmail.com',
-    GOOGLE_PASS: 'portfolio21232',
-    ADMIN_USER: 'jeeasistio08@gmail.com'
-  },
   async rewrites() {
     return [
       {
